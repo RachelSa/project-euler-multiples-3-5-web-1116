4bae786a53bdbbaf4f8af79317eafa544e74110e
github username <github email address>
2017-01-05 21:57:01 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  results = []
+  for num in 1...limit
+    if num % 3 == 0 || num % 5 == 0
+      results << num
+    end
+  end
+    results
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  multiples.reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+def initialize(limit)
+  @limit = limit
+end
+
+def collect_multiples
+  results = []
+  for num in 1...@limit
+    if num % 3 == 0 || num % 5 == 0
+      results << num
+    end
+  end
+    results
+end
+
+def sum_multiples
+  multiples = collect_multiples
+  multiples.reduce(:+)
+end
+end
